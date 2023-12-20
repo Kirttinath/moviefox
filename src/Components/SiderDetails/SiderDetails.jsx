349d417fffc7e0dddef761081120a16f8d49864a
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-20 19:51:04 +0530

    siderview details added and some properties added

---
+++ src/Components/SiderDetails/SiderDetails.jsx
@@ -0,0 +1,15 @@
+import profilepic from "../../assets/k.png";
+
+const SiderDetails = () => {
+  return (
+    <div className="siderdetails">
+      <div className="imagecontainer">
+        <img src={profilepic} alt="" />
+        <h3 className="name">Kirttinath Ojha</h3>
+      </div>
+      <div className="sideritems"></div>
+    </div>
+  );
+};
+
+export default SiderDetails;
