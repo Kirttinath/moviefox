321cd7235ce8d52a8fa6490cf7866799b712aa9b
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-21 19:10:24 +0530

    Add home page design

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,45 @@
-import React from "react";
 import "./Home.scss";
 const Home = () => {
-  return <div></div>;
+  return (
+    <div className="home">
+      <div className="group">
+        <img className="mask" src="" alt="" />
+        <p className="kojha"></p>
+        <img src="" alt="" className="vector1" />
+        <div className="autogroup">
+          <div className="autogroupimg">
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
+          <p className="discoverelements">
+            <span className="discover">
+              Discover
+              <br />
+            </span>
+            <span className="playlist">
+              Playlist
+              <br />
+            </span>
+            <span className="movie">
+              Movie
+              <br />
+            </span>
+            <span className="tv">
+              TV Shows
+              <br />
+            </span>
+            <span className="mylist">
+              My List
+              <br />
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
