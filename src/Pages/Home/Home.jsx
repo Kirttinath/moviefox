911fbaaa5510642bca117e4a2dbbb2aed884f612
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-22 17:10:00 +0530

    Refactor Home page layout and UI elements

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,27 +5,27 @@ import playlist_icon from "../../assets/playlist.png";
 import movie_icon from "../../assets/movie.png";
 import tv_icon from "../../assets/tv.png";
 import list_icon from "../../assets/list.png";
-// import watch_icon from "../../assets/watch.png";
-// import recomend_icon from "../../assets/recomend.png";
-// import setting_icon from "../../assets/setting.png";
-// import logout_icon from "../../assets/logout.png";
+import watch_icon from "../../assets/watch.png";
+import recomend_icon from "../../assets/recomend.png";
+import setting_icon from "../../assets/setting.png";
+import logout_icon from "../../assets/logout.png";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="group">
-        <img className="mask" src={profile} alt="" />
-        <p className="kojha">Kirttinath Ojha</p>
-        <hr className="vector1" />
-        <div className="autogroup">
-          <div className="autogroupimg">
-            <img src={discover_icon} alt="" />
-            <img src={playlist_icon} alt="" />
-            <img src={movie_icon} alt="" />
-            <img src={tv_icon} alt="" />
-            <img src={list_icon} alt="" />
+    <div className="sideview">
+      <div className="autogroup">
+        <img className="mask" src={profile} />
+        <p className="prf">Kirttinath Ojha</p>
+        <img className="vector1" src="" />
+        <div className="groupbox1">
+          <div className="groupelements1">
+            <img className="ds" src={discover_icon} />
+            <img className="pl" src={playlist_icon} />
+            <img className="mv" src={movie_icon} />
+            <img className="tv" src={tv_icon} />
+            <img className="myli" src={list_icon} />
           </div>
-          <p className="discoverelements">
+          <p className="grpelemnames1">
             <span className="discover">
               Discover
               <br />
@@ -37,15 +37,43 @@ const Home = () => {
             <span className="movie">
               Movie
               <br />
-            </span>
-            <span className="tv">
               TV Shows
               <br />
-            </span>
-            <span className="mylist">
               My List
-              <br />
             </span>
+          </p>
+          <div className="rectangle1"></div>
+        </div>
+        <img className="vector2" src="" />
+        <div className="groupbox2">
+          <div className="groupelements2">
+            <img className="wc" src={watch_icon} />
+            <img className="rmd" src={recomend_icon} />
+          </div>
+          <p className="watch">
+            Watch Later
+            <br />
+            Recomended
+          </p>
+        </div>
+        <img className="vector3" src="" />
+        <div className="auto-group-ycwr-wdP" id="A21z5c2dnAzkEFtU6AYCWR">
+          <div className="auto-group-fjft-s1F" id="A21zCBg1CRbg6sZk29fjFT">
+            <img
+              className="vector-Nih"
+              src="/api/prod-ap-southeast-1-first-cluster/projects/8a..."
+              id="40:103"
+            />
+            <img
+              className="vector-vER"
+              src="/api/prod-ap-southeast-1-first-cluster/projects/8a..."
+              id="40:104"
+            />
+          </div>
+          <p className="settings-logout-3pq" id="40:14">
+            Settings
+            <br />
+            Logout
           </p>
         </div>
       </div>
