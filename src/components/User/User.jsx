32fc19488eef0513b88aceffec92552a781146d0
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-22 18:17:40 +0530

    Update user profile picture and styling

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./User.css";
 import user from "../../assets/k.png";
 
-export default function User() {
+const User = () => {
   return (
     <div className="User">
       <img src={user} alt="" />
       <h2>Kirttinath Ojha</h2>
     </div>
   );
-}
+};
+
+export default User;
