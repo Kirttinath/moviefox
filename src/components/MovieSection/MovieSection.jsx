6a86f041ed5f9014dda7f920907ece434341a257
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-22 20:41:07 +0530

    Update package.json and package-lock.json. Remove unnecessary state and code from MovieSection component.

--- src/components/MovieSection/MovieSection.jsx
+++ src/components/MovieSection/MovieSection.jsx
@@ -1,26 +1,22 @@
 import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import "./MovieSection.css";
 
 import useFetch from "../../Hooks/useFetch";
 
 const MovieSection = () => {
-  const [background, setBackground] = useState("");
-  const [query, setQuery] = useState("");
-  const navigate = useNavigate();
-  const { url } = useSelector((state) => state.home);
-  const { data, loading } = useFetch("/movie/upcoming");
-  useEffect(() => {
-    const bg =
-      url.backdrop +
-      data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
-    setBackground(bg);
-  }, [data]);
+  // const [background, setBackground] = useState("");
+  // const [query, setQuery] = useState("");
 
-  const searchQueryHandler = (event) => {
-    if (event.key === "Enter" && query.length > 0) {
-      navigate(`/search/${query}`);
-    }
-  };
+  // const { url } = useSelector((state) => state.home);
+  // const { data, loading } = useFetch("/movie/upcoming");
+  // useEffect(() => {
+  //   const bg =
+  //     url.backdrop +
+  //     data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+  //   setBackground(bg);
+  // }, [data]);
+
   return <div className="MovieSection"></div>;
 };
 
